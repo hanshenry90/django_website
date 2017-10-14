a5ebad704d476c663246ef24462aa4a4f612fb54
hanshenry90 <hanshenry90@gmail.com>
2017-10-14 13:26:40 -0600

    linked db

--- music/views.py
+++ music/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from . models import Album
 
 def index(request): # views function
-    return HttpResponse("<h1>Music</h1>")
+    all_albums=Album.objects.all()
+    html=''
+    for i in all_albums:
+        url='/music/' + str(i.id) + '/'
+        html += '<a href="' + url + '">' + i.album_title + '</a><br>'
+        # html += '<a href="' + url + '">' + str(i.id) + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Detailes for Album id: " + str(album_id) + "</h2>")

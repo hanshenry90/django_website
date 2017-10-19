b443d21b30aa57b19279d0fe203d66de92f774c1
hanshenry90 <hanshenry90@gmail.com>
2017-10-19 10:56:16 -0600

    hard code

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name ='music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'), # request nothing ^$
 
-    # /music/712/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /music/<album_id>/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), # 'detail' is the url request pattern: r'^(?P<album_id>[0-9]+)/$', now go find your response in views.detail
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,9 @@
 # from django.http import HttpResponse
-from django.http import Http404
+# from django.http import Http404
 # from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Album
+
 
 def index(request): # views function
 
@@ -22,9 +23,11 @@ def index(request): # views function
 def detail(request, album_id):
 
     # return HttpResponse("<h2>Detailes for Album id: " + str(album_id) + "</h2>")
-    try:
-        album=Album.objects.get(pk=album_id)
+    # try:
+    #     album=Album.objects.get(pk=album_id)
+    #
+    # except Album.DoesNotExist:
+    #     raise Http404("Album does not exist!")
 
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist!")
-    return render(request, 'music/detail.html', {'cao':album})
+    album=get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album':album})

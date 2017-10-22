e288586a79369754c245c26aa39ebd88a7656609
hanshenry90 <hanshenry90@gmail.com>
2017-10-22 13:37:49 -0600

    added side button & search bar

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),  # django looks for pattern that matches url
     url(r'^music/', include('music.urls')) # the request of music url has a response: music/urls.py, include(file.py)
 ]
+

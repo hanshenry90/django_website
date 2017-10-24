9e748210bde0a69c331278ae492d3f51c1b7a2cd
hanshenry90 <hanshenry90@gmail.com>
2017-10-24 09:27:38 -0600

    added album_form.html

--- music/urls.py
+++ music/urls.py
@@ -5,10 +5,13 @@ app_name ='music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'), # request nothing ^$
+    url(r'^$', views.IndexView.as_view(), name='index'), # request nothing ^$
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), # 'detail' is the url request pattern: r'^(?P<album_id>[0-9]+)/$', now go find your response in views.detail
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), # 'detail' is the url request pattern: r'^(?P<album_id>[0-9]+)/$', now go find your response in views.detail
     # /music/<album_id>/favoite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+    # url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+
+    # /music/album/add/    ...a form generated
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
 ]
--- music/views.py
+++ music/views.py
@@ -1,47 +1,68 @@
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from . models import Album
+
+class IndexView(generic.ListView):
+    template_name='music/index.html'
+    context_object_name='all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model=Album
+    template_name='music/detail.html'
+
+# a Model form generating
+class AlbumCreate(CreateView):
+    model=Album
+    fields=['artist', 'album_title', 'genre', 'album_logo']
+
+
 # from django.http import HttpResponse
 # from django.http import Http404
 # from django.template import loader
-from django.shortcuts import render, get_object_or_404
-from . models import Album
+# from django.shortcuts import render, get_object_or_404
+# from . models import Album
 
 
-def index(request): # views function
-
-    all_albums=Album.objects.all()
-    # template = loader.get_template('music/index.html') # Provides templates to views.py
-    context = {
-        'all_albums': all_albums # A dictionary of the Album objects in database that the template needs to work behinde scene
-    }
-    return render(request, 'music/index.html', context) # A built in HttpResponse in render()
-
-    # html=''
-    # for i in all_albums:
-    #     url='/music/' + str(i.id) + '/'
-    #     html += '<a href="' + url + '">' + i.album_title + '</a><br>'
-    # return HttpResponse(template.render(context, request)) # Give the response to user's request in url
-
-def detail(request, album_id):
-
-    # return HttpResponse("<h2>Detailes for Album id: " + str(album_id) + "</h2>")
-    # try:
-    #     album=Album.objects.get(pk=album_id)
-    #
-    # except Album.DoesNotExist:
-    #     raise Http404("Album does not exist!")
-
-    album=get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album':album}) # model access, album class in return
-
-def favorite(request, album_id):
-    album=get_object_or_404(Album,pk=album_id)
-    try:
-        selected_song=album.song_set.get(pk=request.POST['song'])
-    except (KeyError, song.DoesNotExist):
-        return(request, 'music/detail.html', {'album':album,
-                                              'error_message':"You did not select a valid song."})
-
-    else:
-        selected_song.is_favorite=True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album':album})
+# def index(request): # views function
+#
+#     all_albums=Album.objects.all()
+#     # template = loader.get_template('music/index.html') # Provides templates to views.py
+#     context = {
+#         'all_albums': all_albums # A dictionary of the Album objects in database that the template needs to work behinde scene
+#     }
+#     return render(request, 'music/index.html', context) # A built in HttpResponse in render()
+#
+#     # html=''
+#     # for i in all_albums:
+#     #     url='/music/' + str(i.id) + '/'
+#     #     html += '<a href="' + url + '">' + i.album_title + '</a><br>'
+#     # return HttpResponse(template.render(context, request)) # Give the response to user's request in url
+#
+# def detail(request, album_id):
+#
+#     # return HttpResponse("<h2>Detailes for Album id: " + str(album_id) + "</h2>")
+#     # try:
+#     #     album=Album.objects.get(pk=album_id)
+#     #
+#     # except Album.DoesNotExist:
+#     #     raise Http404("Album does not exist!")
+#
+#     album=get_object_or_404(Album, pk=album_id)
+#     return render(request, 'music/detail.html', {'album':album}) # model access, album class in return
+#
+# def favorite(request, album_id):
+#     album=get_object_or_404(Album,pk=album_id)
+#     try:
+#         selected_song=album.song_set.get(pk=request.POST['song'])
+#     except (KeyError, song.DoesNotExist):
+#         return(request, 'music/detail.html', {'album':album,
+#                                               'error_message':"You did not select a valid song."})
+#
+#     else:
+#         selected_song.is_favorite=True
+#         selected_song.save()
+#         return render(request, 'music/detail.html', {'album':album})
 
